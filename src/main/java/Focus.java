d7ad1f089dbcdf3ae5e5a7e2ec7be062b70cb106
Angmar2722 <64848809+Angmar2722@users.noreply.github.com>
2025-08-26 15:49:21 +0800

    Finish Level-0

---
+++ src/main/java/Focus.java
@@ -0,0 +1,15 @@
+public class Focus {
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printLine();
+        System.out.println(" Hello! I'm Focus\n" + " What can I do for you?");
+        printLine();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+}
