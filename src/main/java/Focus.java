ca267d2b2b98c3775793dd5eda1cfdb11c801894
Angmar2722 <64848809+Angmar2722@users.noreply.github.com>
2025-08-26 16:30:50 +0800

    Finish Level-1

--- src/main/java/Focus.java
+++ src/main/java/Focus.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Focus {
 
     public static void printLine() {
@@ -5,11 +7,30 @@ public class Focus {
     }
 
     public static void main(String[] args) {
+
         printLine();
         System.out.println(" Hello! I'm Focus\n" + " What can I do for you?");
         printLine();
-        System.out.println(" Bye. Hope to see you again soon!");
-        printLine();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            String echoInput = scanner.nextLine();
+
+            printLine();
+
+            if (echoInput.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            } else {
+                System.out.println(echoInput);  //Echo user command
+                printLine();
+            }
+
+        }
+
     }
 
 }
