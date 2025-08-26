d737ce9e78d18b5e8bac074f6f751336c076eb16
Angmar2722 <64848809+Angmar2722@users.noreply.github.com>
2025-08-26 18:15:40 +0800

    Finish Level-2

--- src/main/java/Focus.java
+++ src/main/java/Focus.java
@@ -1,31 +1,42 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Focus {
 
+    /**
+     * prints a horizontal line of dashes
+     */
     public static void printLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
 
         printLine();
-        System.out.println(" Hello! I'm Focus\n" + " What can I do for you?");
+        System.out.println("    Hello! I'm Focus\n" + "    What can I do for you?");
         printLine();
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
         while (true) {
 
-            String echoInput = scanner.nextLine();
+            String input = scanner.nextLine();
 
             printLine();
 
-            if (echoInput.equals("bye")) {
-                System.out.println(" Bye. Hope to see you again soon!");
+            if (input.equals("bye")) {
+                System.out.println("     Bye. Hope to see you again soon!");
                 printLine();
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("     %d. %s\n", i + 1, tasks.get(i));
+                }
+                printLine();
             } else {
-                System.out.println(echoInput);  //Echo user command
+                System.out.printf("     added: %s\n", input);
+                tasks.add(input);
                 printLine();
             }
 
